5a7cec76020317052bafb5460f7981b07ac8cebd
nickregeda <kolyareggeda@gmail.com>
2024-08-14 14:16:37 +0300

    feat: add book model

---
+++ src/app/models/book.model.ts
@@ -0,0 +1,13 @@
+export interface IBook {
+  id: number;
+  title: string;
+  author: string;
+  publishedYear: number;
+  genre: string;
+  pageCount: number;
+  publisher: string;
+  language: string;
+  description: string;
+  rating: number;
+  coverImageUrl: string;
+}
