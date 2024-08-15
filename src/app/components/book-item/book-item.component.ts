4338b980bb7e2dba380c9e23a7b49102247805b8
nickregeda <kolyareggeda@gmail.com>
2024-08-16 01:49:39 +0300

    fix: disable edit mode on close popup

--- src/app/components/book-item/book-item.component.ts
+++ src/app/components/book-item/book-item.component.ts
@@ -1,9 +1,11 @@
-import { ChangeDetectionStrategy, Component, inject, input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, DestroyRef, inject, input } from '@angular/core';
 import { NgOptimizedImage } from '@angular/common';
 import { MatDialog } from '@angular/material/dialog';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 import { BookPopupComponent } from '../book-popup/book-popup.component';
 import { IBook } from '../../models/book.model';
+import { BooksService } from '../../services/books.service';
 
 @Component({
   selector: 'app-book-item',
@@ -15,12 +17,19 @@ import { IBook } from '../../models/book.model';
 })
 export class BookItemComponent {
   private matDialog = inject(MatDialog);
+  private destroyRef = inject(DestroyRef);
+  private booksService = inject(BooksService);
+
   public book = input.required<IBook>();
 
   public openBookInfo(): void {
-    this.matDialog.open(BookPopupComponent, {
+    const dialogRef = this.matDialog.open(BookPopupComponent, {
       data: this.book,
       panelClass: 'mat-book-popup',
     });
+
+    dialogRef.afterClosed()
+    .pipe(takeUntilDestroyed(this.destroyRef))
+    .subscribe(() => this.booksService.toggleEditMode(false));
   }
 }
