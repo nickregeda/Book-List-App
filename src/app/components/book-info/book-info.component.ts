4501f15da30e47d40d0950a333b77fa53ae03e2a
nickregeda <kolyareggeda@gmail.com>
2024-08-16 01:23:04 +0300

    feat: add book-info component

---
+++ src/app/components/book-info/book-info.component.ts
@@ -0,0 +1,24 @@
+import { Component, inject, input } from '@angular/core';
+import { MatButton } from '@angular/material/button';
+
+import { BOOK_FORM_ELEMENTS } from '../book-form/book-form';
+import { BooksService } from '../../services/books.service';
+import { IBook } from '../../models/book.model';
+
+@Component({
+  selector: 'app-book-info',
+  standalone: true,
+  templateUrl: './book-info.component.html',
+  styleUrl: './book-info.component.scss',
+  imports: [
+    MatButton,
+  ],
+})
+export class BookInfoComponent {
+  private booksService = inject(BooksService);
+  public book = input.required<IBook>();
+
+  public enableEditMode(): void {
+    this.booksService.toggleEditMode(true);
+  }
+}
