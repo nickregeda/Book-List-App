06ba29bbfc1c1a47f60839c8544d806d4cc778ad
nickregeda <kolyareggeda@gmail.com>
2024-08-16 17:04:13 +0300

    feat: update book-form for 'add-book' feature

--- src/app/components/book-form/book-form.component.ts
+++ src/app/components/book-form/book-form.component.ts
@@ -1,8 +1,9 @@
-import { Component, inject, input, OnInit } from '@angular/core';
+import { Component, computed, inject, input, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 import { MatInput } from '@angular/material/input';
 import { MatButton } from '@angular/material/button';
+import { MatDialogRef } from '@angular/material/dialog';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { MatFormField, MatLabel } from '@angular/material/form-field';
 
@@ -24,47 +25,40 @@ import { BooksService } from '../../services/books.service';
   ],
 })
 export class BookFormComponent implements OnInit {
-  public book = input.required<IBook>();
+  public book = input<IBook>();
 
   private fb = inject(FormBuilder);
   private matSnackBar = inject(MatSnackBar);
+  private matDialogRef = inject(MatDialogRef);
   private booksService = inject(BooksService);
+
+  private isAddBookPopup = computed(() => !this.book());
+  private coverImage: string;
 
   public bookFormEls = BOOK_FORM_ELEMENTS;
   public bookForm: FormGroup;
 
   ngOnInit() {
-    const {
-      title,
-      author,
-      publishedYear,
-      genre,
-      publisher,
-      language,
-      description,
-    } = this.book();
-
     this.bookForm = this.fb.group({
-      title: [ title, Validators.required ],
-      author: [ author, Validators.required ],
-      publishedYear: [ publishedYear, Validators.required ],
-      genre: [ genre, Validators.required ],
-      publisher: [ publisher, Validators.required ],
-      language: [ language, Validators.required ],
-
-      description: [ description ],
+      title: [ this.book()?.title || '', Validators.required ],
+      author: [ this.book()?.author || '', Validators.required ],
+      publishedYear: [ this.book()?.publishedYear || null, Validators.required ],
+      genre: [ this.book()?.genre || '', Validators.required ],
+      publisher: [ this.book()?.publisher || '', Validators.required ],
+      language: [ this.book()?.language || '', Validators.required ],
+      description: [ this.book()?.description || '' ],
     });
   }
 
-  public get isFormChanged(): boolean {
-    return this.bookForm.dirty;
+  private closePopup(): void {
+    this.matDialogRef.close();
   }
 
-  public disableEditMode(): void {
+  private disableEditMode(): void {
     this.booksService.toggleEditMode(false);
   }
 
-  public saveBook(): void {
+  private updateBook(): void {
     const updatedBook: IBook = {
       ...this.book(),
       ...this.bookForm.value,
@@ -73,5 +67,51 @@ export class BookFormComponent implements OnInit {
     this.booksService.updateBook(updatedBook);
     this.matSnackBar.open("Book's info updated!", "OK");
     this.disableEditMode();
+  }
+
+  private addBook(): void {
+    const newBook: Omit<IBook, 'id'> = {
+      ...this.bookForm.value,
+      coverImageUrl: this.coverImage,
+    };
+
+    this.booksService.createBook(newBook);
+    this.matSnackBar.open(`Book ${ newBook.title } created!`, "OK");
+    this.closePopup();
+  }
+
+  public get disabled(): boolean {
+    if (this.isAddBookPopup()) {
+      return this.bookForm.invalid;
+    } else {
+      return !this.bookForm.dirty;
+    }
+  }
+
+  public onImageUpload(event: Event): void {
+    const inputEl = event.target as HTMLInputElement;
+    if (inputEl.files?.length) {
+      const file = inputEl.files[0];
+      const reader = new FileReader();
+
+      reader.onload = () => this.coverImage = reader.result as string;
+      reader.readAsDataURL(file);
+    }
+  }
+
+  public cancel(): void {
+    if (this.isAddBookPopup()) {
+      this.closePopup();
+    } else {
+      this.disableEditMode();
+    }
+  }
+
+  public submit(): void {
+    if (this.isAddBookPopup()) {
+      this.addBook();
+    } else {
+      this.updateBook();
+    }
   }
 }
