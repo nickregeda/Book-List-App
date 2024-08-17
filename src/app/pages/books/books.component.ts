97453213a914244f8542c045921ec7eeaf3e0892
nickregeda <kolyareggeda@gmail.com>
2024-08-17 08:47:26 +0300

    feat: add search feature

--- src/app/pages/books/books.component.ts
+++ src/app/pages/books/books.component.ts
@@ -1,9 +1,14 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, DestroyRef, ElementRef, inject, OnInit, viewChild } from '@angular/core';
 import { animate, style, transition, trigger } from '@angular/animations';
-import { MatProgressBar } from '@angular/material/progress-bar';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { debounceTime, distinctUntilChanged, fromEvent, map } from 'rxjs';
+
 import { MatIcon } from '@angular/material/icon';
+import { MatInput } from '@angular/material/input';
 import { MatButton } from '@angular/material/button';
 import { MatDialog } from '@angular/material/dialog';
+import { MatProgressBar } from '@angular/material/progress-bar';
+import { MatFormField, MatLabel } from '@angular/material/form-field';
 
 import { BooksService } from '../../services/books.service';
 import { BookItemComponent } from '../../components/book-item/book-item.component';
@@ -27,19 +32,37 @@ import { BookFormComponent } from '../../components/book-form/book-form.componen
   ],
   imports: [
     MatIcon,
+    MatInput,
+    MatLabel,
     MatButton,
+    MatFormField,
     MatProgressBar,
+
     BookItemComponent,
   ],
 })
 export class BooksComponent implements OnInit {
   private matDialog = inject(MatDialog);
+  private destroyRef = inject(DestroyRef);
   private booksService = inject(BooksService);
+
+  private searchInputEl = viewChild<ElementRef>('searchInput');
   public books = this.booksService.books;
   public loading = this.booksService.loading;
 
   ngOnInit() {
     this.booksService.loadBooks();
+    this.subscribeOnSearch();
+  }
+
+  private subscribeOnSearch(): void {
+    fromEvent<Event>(this.searchInputEl().nativeElement, 'input')
+    .pipe(
+      map((event) => (event.target as HTMLInputElement).value),
+      takeUntilDestroyed(this.destroyRef),
+      debounceTime(500),
+      distinctUntilChanged(),
+    ).subscribe((query) => this.booksService.searchBook(query));
   }
 
   public openAddBookPopup(): void {
